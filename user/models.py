b190d1d03b7c47c7377a53a643c9bf41951782fb
Facundo Dalto <facundodalto@gmail.com>
2022-06-27 18:08:57 -0300

    prueba3

--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Avatar(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='staticfiles/avatars', null=True, blank=True)
+    image = models.ImageField(upload_to='staticfiles/avatars', null=True, blank=True,default="")
 
     def __str__(self):
         return f'url: {self.image.url}'
